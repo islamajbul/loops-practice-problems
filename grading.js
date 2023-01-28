d4e3df60708b6fd51f1ff8770a0d9c0efdf7b499
islamajabul <ajabulmd1@gmail.com>
2023-01-28 21:49:52 +0800

    largest number solved

---
+++ grading.js
@@ -0,0 +1,7 @@
+var myMarks = 85;
+var tomsMarks = 66;
+var janesMarks = 95;
+var petarsMarks = 56;
+var johnsMarks = 40;
+
+
---
+++ largest.js
@@ -0,0 +1,20 @@
+var num1 = 113;
+var num2 = 179;
+var num3 = 145;
+
+if(num1>num2){
+    if(num1 > num3){
+        console.log(num1);
+    }
+    else{
+        console.log(num3);
+    }
+}
+else{
+    if(num2 > num3){
+        console.log(num2);
+    }
+    else{
+        console.log(num3);
+    }
+}
