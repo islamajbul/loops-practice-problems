607ec614e9592468bdc165f662269bd8c8bd3106
islamajabul <ajabulmd1@gmail.com>
2023-01-28 21:35:22 +0800

    Array-updated

---
+++ problem-array-update.js
@@ -0,0 +1,9 @@
+var fruits = ['Apple','Banana','Orange'];
+var bananaIndex = fruits.indexOf('Banana');
+console.log(bananaIndex);
+fruits[1] = 'Mango'
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push('Watermelon')
+console.log(fruits);
