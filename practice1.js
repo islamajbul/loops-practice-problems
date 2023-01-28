968a14c2e1d03cfd4c72dbcfdb9ef47998366b83
islamajabul <ajabulmd1@gmail.com>
2023-01-28 21:01:16 +0800

    problem1 solved

---
+++ practice1.js
@@ -0,0 +1,8 @@
+var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var expense = applePrice + orangePrice;
+console.log(expense);
+var giveBack = moneyGiven - expense;
+console.log(giveBack);
